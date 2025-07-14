91235954a3b0306d32e9b5e64108fd25d275c9e5
Axel Rosenquist <Axel.Rosenquist4@gmail.com>
2025-07-14 10:19:18 +0200

    Added post and get for the checkboxes

--- app.py
+++ app.py
@@ -4,12 +4,17 @@ import json
 
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
 DATA_FILE = "checkboxes.json"
 
-@app.get("/state")
+@app.get("/get-state")
 async def get_state():
     with open(DATA_FILE, "r") as file:
         data = json.load(file)
     return data
+
+@app.post("/set-state")
+async def set_state(state: dict):
+    with open(DATA_FILE, "w") as file:
+        json.dump(state, file)
--- static/script.js
+++ static/script.js
@@ -1,3 +1,28 @@
+const checkboxes = ['morning', 'day', 'evening'];
+
+
+
 async function fetchCheckboxes(){
-    
+    const response = await fetch('/get-state');
+    const data = await response.json();
+    checkboxes.forEach(checkbox => {
+        document.getElementById(checkbox).checked = data[checkbox];
+    });
 }
+
+setInterval(fetchCheckboxes, 10000);
+fetchCheckboxes();
+
+
+checkboxes.forEach(checkbox => {
+    document.getElementById(checkbox).addEventListener('change', async (event) => {
+        const state = {}
+        checkboxes.map(element => state[element] = document.getElementById(element).checked); 
+        await fetch('/set-state', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(state)
+        });
+    });
+});
+
